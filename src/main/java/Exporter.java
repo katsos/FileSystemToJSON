93da7b1e9fa180da6965f6bfde1c3b1cf6b6c403
Nikos Katsos <nikos.katsos@persado.com>
2016-12-06 17:43:57 +0200

    removed comments from Exporter class

--- src/main/java/Exporter.java
+++ src/main/java/Exporter.java
@@ -7,48 +7,48 @@ public class Exporter {
 
     public static File file;
 
-//    public static void export(String text) {
-//        export(text, Main.getSourceFile().getName() + ".json");
-//    }
-//
-//    public static void export(String data, String filepath) {
-//        file = new File(filepath);
-//
-//        try {
-//            System.out.println("The file " + file.getAbsolutePath() +
-//                (file.createNewFile() ? " is created" : " already exists") );
-//        } catch (IOException ex) {
-//            System.err.println("Couldn't create " +  file.getAbsolutePath()
-//                + " file.");
-//            System.exit(3001);
-//        }
-//
-//        try (FileWriter writer = new FileWriter(file)) {
-//            writer.write(data);
-//            writer.close();
-//        } catch (IOException ex) {
-//            System.err.println("Couldn't write in " +  file.getAbsolutePath()
-//                + " file.");
-//            System.exit(3002);
-//        }
-//
-//    }
-//
-//    public static void display() {
-//
-//        if( file == null ) {
-//            System.err.println("The file isn't exported yet, "
-//                    + "so it cannot be displayed! ");
-//            return;
-//        }
-//
-//        try {
-//            Desktop desktop = Desktop.getDesktop();
-//            desktop.browse(file.toURI());
-//        } catch (IOException ex) {
-//            System.err.println("Couldn't open " +  file.getAbsolutePath());
-//            System.exit(3003);
-//        }
-//    }
+    public static void export(String text) {
+        export(text, Main.getSourceFile().getName() + ".json");
+    }
+
+    public static void export(String data, String filepath) {
+        file = new File(filepath);
+
+        try {
+            System.out.println("The file " + file.getAbsolutePath() +
+                (file.createNewFile() ? " is created" : " already exists") );
+        } catch (IOException ex) {
+            System.err.println("Couldn't create " +  file.getAbsolutePath()
+                + " file.");
+            System.exit(3001);
+        }
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(data);
+            writer.close();
+        } catch (IOException ex) {
+            System.err.println("Couldn't write in " +  file.getAbsolutePath()
+                + " file.");
+            System.exit(3002);
+        }
+
+    }
+
+    public static void display() {
+
+        if( file == null ) {
+            System.err.println("The file isn't exported yet, "
+                    + "so it cannot be displayed! ");
+            return;
+        }
+
+        try {
+            Desktop desktop = Desktop.getDesktop();
+            desktop.browse(file.toURI());
+        } catch (IOException ex) {
+            System.err.println("Couldn't open " +  file.getAbsolutePath());
+            System.exit(3003);
+        }
+    }
 
 }
