39d7c18f57396cb55c626a65bbe5df39d1bbf751
Nikos Katsos <nikos.katsos@persado.com>
2016-12-28 19:43:59 +0200

    gave option to user to avoid overwritting the selected  export-file

--- src/main/java/Exporter.java
+++ src/main/java/Exporter.java
@@ -4,10 +4,12 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Exporter {
 
     public static File file;
+    private static JSONObject jsonObject;
 
     public static void export(JSONObject jsonToString) {
         export(jsonToString, Main.getSourceFile().getAbsolutePath() + ".json");
@@ -17,32 +19,17 @@ public class Exporter {
 
         if (jsonObject == null) {
             System.err.println("No data to display");
-            System.exit(5000);
+            System.exit(500);
         }
 
+        Exporter.jsonObject = jsonObject;
         file = new File(filepath);
 
-        try {
-            System.out.println("The file " + file.getAbsolutePath() +
-                (file.createNewFile() ? " is created" : " already exists") );
-        } catch (IOException ex) {
-            // TODO: give user the option to check other available filepaths
-            // to save his file instead of terminating the program
-            System.err.println("Couldn't create " +  file.getAbsolutePath() + " file.");
-            System.exit(3001);
-        }
+        askUserForOverwrite();
 
-        try {
-            FileWriter writer = new FileWriter(file);
-            writer.write(jsonObject.toString(2));
-            writer.close();
-        } catch (IOException ex) {
-            System.err.println("Couldn't write into " +  file.getAbsolutePath() + " file.");
-            System.exit(3002);
-        } catch (Exception ex) {
-            System.err.println(ex.toString());
-        }
+        createExportFile();
 
+        writeFile();
     }
 
     public static void display() {
@@ -58,6 +45,54 @@ public class Exporter {
         } catch (IOException ex) {
             System.err.println("Couldn't open " +  file.getAbsolutePath());
             System.exit(3003);
+        }
+    }
+
+    private static void askUserForOverwrite() {
+        String answer;
+
+        if ( !file.exists() ) {
+            return;
+        }
+
+        System.out.println("The file " + file.getAbsoluteFile() + " already exists.");
+        System.out.println("Do you want to overwrite it? (y/n)");
+
+        Scanner scanner = new Scanner(System.in);
+        answer = scanner.nextLine().toLowerCase();
+
+        while ( answer.equals("y") && answer.equals("n") ) {
+            System.err.println("Invalid answer. Press \"y\" for yes or \"n\" for no.");
+            System.out.println("Do you want to overwrite it? (y/n)");
+            answer = scanner.nextLine().toLowerCase();
+        }
+
+        if ( answer.equals("n") ) {
+            System.exit(0);
+        }
+    }
+
+    private static void createExportFile() {
+        try {
+            file.createNewFile();
+        } catch (IOException ex) {
+            System.err.println("Couldn't create " +  file.getAbsolutePath() + " file.");
+            // TODO: give user the option to check other available filepaths
+            // to save his file instead of terminating the program
+            System.exit(3001);
+        }
+    }
+
+    private static void writeFile() {
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(jsonObject.toString(Arguments.getIndentation()));
+            writer.close();
+        } catch (IOException ex) {
+            System.err.println("Couldn't write into " +  file.getAbsolutePath() + " file.");
+            System.exit(3002);
+        } catch (Exception ex) {
+            System.err.println(ex.toString());
         }
     }
 
