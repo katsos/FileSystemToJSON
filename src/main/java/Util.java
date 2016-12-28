3853d780f7413b230e419a2979a8a5f345c668fa
Nikos Katsos <nikos.katsos@persado.com>
2016-12-28 20:08:25 +0200

    minor - removed unused statement

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -2,8 +2,6 @@ public class Util {
 
     public static boolean contains (String[] array, String theChosenOne ) {
 
-        if ( array.length == 0 ) return false;
-
         for (String s : array) {
             if (s.equals(theChosenOne)) return true;
         }
